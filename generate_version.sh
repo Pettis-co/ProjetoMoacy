887fc4cd2e01b8cc96c4699de7d376db43d832fe
Igor Oliveira <igor.oliveira@ccc.ufcg.edu.br>
2025-01-26 20:21:48 -0300

    :books: fix syntax errors

--- generate_version.sh
+++ generate_version.sh
@@ -4,14 +4,14 @@
 
 #!/bin/bash
 
-if [ -z  $(cat version.txt 2> /dev/null) ]; then
+if [ -z  "$(cat version.txt 2> /dev/null)" ]; then
 	VERSION="1"
     else
 	VERSION=$(cat version.txt)  
 fi
 IMAGE_NAME="local/petis.co"
 
-CURRENT_VERSION=$IMAGE_NAME:v0.$VERSION 
+CURRENT_VERSION="$IMAGE_NAME:v0.$VERSION"
 
 #increments the version counter
 echo $((VERSION + 1)) > version.txt
